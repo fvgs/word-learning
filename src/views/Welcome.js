a3b9d73c2736c1b59a5eed3509978b7919c5d4fe
Felipe Vargas <git@fvgs.ai>
2018-11-17 23:01:27 -0500

    Fix typo and adjust time information on welcome view

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -20,10 +20,10 @@ const Welcome = ({setView}) => {
 					<div>You will be presented with a number of trials each consisting of two pseudowords and two objects. Your goal is to correctly learn the association between a word and its corresponding object.</div>
 					<ul style={{marginBottom: '2.5rem'}}>
 						<li>There are 18 words and 18 objects.</li>
-						<li>Each word corresonds to a single object.</li>
+						<li>Each word corresponds to a single object.</li>
 						<li>There are 54 trials, each lasting 6 seconds.</li>
 						<li>Please do not take any notes during the trials.</li>
-						<li>Please focus on the task for the duration of the study (~5 min)</li>
+						<li>Please focus on the task for the duration of the study (7 minutes)</li>
 						<li>Please complete the word-object association assessment following the final trial as soon as the last trial has ended.</li>
 						<li>Please do not complete the study more than once.</li>
 					</ul>
